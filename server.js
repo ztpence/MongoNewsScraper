4cdc43f795736f716f6e8a9aa13850d633fc5a76
Zane P <ztpence@gmail.com>
2018-06-08 23:10:45 -0700

    basic file structure and server.js work set up main.handlebars html

---
+++ server.js
@@ -0,0 +1,43 @@
+// npm packages
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
+const cheerio = require("cheerio");
+const axios = require("axios");
+const logger = require("morgan");
+
+const db = require('./models');
+
+let PORT = 5000;
+
+const app = express();
+
+//Middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.jason());
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/");
+
+// setup handlebars view main folder default
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+
+
+
