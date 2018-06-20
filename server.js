2b1804307e04b9932856a94f8e798eacfb07561e
Zane P <ztpence@gmail.com>
2018-06-19 17:27:42 -0700

    added to server.js

--- server.js
+++ server.js
@@ -43,7 +43,44 @@ app.get('/', function(req, res){
     res.render('index')
 });
 
+app.get('/scrape', function(req, res){
+    // First, we grab the body of the html with request
+    axios.get('').then(function(response){
+        // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
 
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("").each(function(i, element) {
+        // Save an empty result object
+        var result ={};
+
+    // Add the text and href of every link, and save them as properties of the result object
+    result.title = $(this)
+      .children("a")
+      .attr("title");
+    result.link = $(this)
+      .children("a")
+      .attr("href");
+    result.img = $(this)
+        .children("a")  
+        .children("img")
+        .atrr("src");
+    
+        db.Article.create(result)
+        .then(function(dbArticle) {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          // If an error occurred, send it to the client
+          return res.json(err);
+        });
+    })
+
+    // If we were able to successfully scrape and save an Article, send a message to the client
+    res.redirect("/articles");
+   });
+});
 
 
 
